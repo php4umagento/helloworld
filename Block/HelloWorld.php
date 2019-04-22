ec313c7f49ce4d50e7b1d320c9ca8033453f0638
mage designer <zalavadiyahardik14993@gmail.com>
2019-04-22 16:16:02 +0530

    first commit

---
+++ Block/HelloWorld.php
@@ -0,0 +1,16 @@
+<?php
+namespace LumbralesSoftware\HelloWorld\Block;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * HelloWorld
+ *
+ * @package
+ * @version $id$
+ * @copyright 2015, Lumbrales Software
+ * @author Javier Carrascal <info@lumbrales-software.com>
+ */
+class HelloWorld extends Template
+{
+}
